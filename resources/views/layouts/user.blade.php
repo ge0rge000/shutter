769698c07cdfea435544a916db5c7e53428fb14c
george samy <gogosony124@gmail.com>
2024-05-28 17:55:20 +0400

    asdasd

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -31,22 +31,34 @@
     <!-- Template Stylesheet -->
     <link href="{{ asset('user/css/style.css') }}" rel="stylesheet">
     @livewireStyles
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PZDHJSGS');</script>
+        <!-- End Google Tag Manager -->
     @if (isset($schemaData))
     <script type="application/ld+json">
         {!! $schemaData !!}
     </script>
     @endif
+    <!-- Google Tag Manager -->
+
 </head>
 
 
 <body>
+
 <style>
     img.logo {
     filter: invert(1);
     margin-bottom: 8px;
 }
 </style>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZDHJSGS"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <livewire:user.items.whatsappitems/>
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg bg-white navbar-light shadow border-top border-5 border-primary sticky-top p-0">
